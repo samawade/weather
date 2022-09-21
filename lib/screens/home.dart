fe30d3d009ec45c42c7db24bed23d1d2d9c98d83
Samir Samawade <samirsamawade@gmail.com>
2022-09-21 14:26:09 +0300

    Home Body Text

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -55,7 +55,23 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Welcome to \n Weather App ⛅"),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: "Welcome to ",
+                    style: TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.w300, height: 1.5),
+                    children: [
+                      TextSpan(
+                        text: "\n Weather App ⛅",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
